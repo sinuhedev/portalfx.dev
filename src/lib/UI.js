c15e689008379f54c575de30742aa5d0066df265
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-06-16 16:46:32 -0600

    Fixed css theme

--- src/lib/UI.js
+++ src/lib/UI.js
@@ -1,12 +1,14 @@
-import { onResize } from './Util'
+import { isMobile, onResize } from './Util'
 
 class UI {
   winPause = document.getElementById('win-pause')
   btnLogo = document.getElementById('btn-logo')
   btnContinue = document.getElementById('btn-continue')
+  btnFullscreen = document.getElementById('btn-fullscreen')
 
   constructor() {
     this.#resize()
+    this.#events()
   }
 
   #resize() {
@@ -15,6 +17,20 @@ class UI {
         this.btnLogo.style.display = 'block'
       } else {
         this.btnLogo.style.display = 'none'
+      }
+    })
+  }
+
+  #events() {
+    this.btnFullscreen.addEventListener('click', async () => {
+      if (!document.fullscreenElement) {
+        await document.documentElement.requestFullscreen()
+      } else {
+        await document.exitFullscreen()
+      }
+
+      if ('orientation' in screen && isMobile()) {
+        await screen.orientation.lock('landscape')
       }
     })
   }
